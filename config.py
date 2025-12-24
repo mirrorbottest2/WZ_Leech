6d406ab450ba39fe6665a7ba24e126fc1814cc38
mirrorbottest2 <110727882+mirrorbottest2@users.noreply.github.com>
2025-12-24 13:30:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -175,7 +175,7 @@ MIRROR_LOG_ID = ""
 # qBittorrent/Aria2c
 TORRENT_TIMEOUT = 90000
 #BASE_URL = "https://popular-rachel-twisted-fairy-94093a16.koyeb.app/"
-BASE_URL = "http://arrogant-frederica-testarthav69-41482dbe.koyeb.app/"
+BASE_URL = "http://printed-aurel-natashalikebbc-5bb1542d.koyeb.app/"
 BASE_URL_PORT = 80
 WEB_PINCODE = False
 
